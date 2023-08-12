f4fae602c375c258eaf15a72666f02c412a50d40
irfanito <57394327+irfanito@users.noreply.github.com>
2023-08-12 16:52:10 +0200

    Met en place un reactive form

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,26 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
-  `,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './main.html',
 })
-export class App {
-  name = 'Angular';
+export class App implements OnInit {
+  public formGroup = new FormGroup({});
+  public ngOnInit(): void {
+    console.log('irf');
+    this.formGroup.addControl('base', new FormControl('17'));
+  }
 }
 
 bootstrapApplication(App);

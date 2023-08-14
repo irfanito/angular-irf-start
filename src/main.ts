6e958d01131c34e2cab4fecaae07b19e62ad84cc
irfanito <57394327+irfanito@users.noreply.github.com>
2023-08-14 02:23:37 +0200

    retour au main.Ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+})
+export class App implements OnInit {
+  public formGroup = new FormGroup({});
+  public ngOnInit(): void {
+    console.log('irf');
+    this.formGroup.addControl('base', new FormControl('17'));
+  }
+}
+
+bootstrapApplication(App);
